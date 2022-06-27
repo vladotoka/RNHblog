3da4062d02d3c5a0f0c062fc081f60cad8442308
vladotoka <vladimir.k.terziev@tutanota.com>
2022-06-27 17:49:35 +0300

    implement useReducer

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,14 +3,14 @@ import React, { useContext } from 'react';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const blogPosts = useContext(BlogContext);
+  const {data, addBlogPost} = useContext(BlogContext);
 
   return (
     <View>
       <Text>IndexScreen</Text>
-      <Button title='нов пост' onPress={blogPosts.addBlogPost}/>
+      <Button title='нов пост' onPress={addBlogPost}/>
       <FlatList
-        data={blogPosts.data}
+        data={data}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => (<Text>{item.title}</Text>)} />
     </View >
