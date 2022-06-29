4a94033ec7ffe5cb52366f9e728ea57b9c37d23b
vladotoka <vladimir.k.terziev@tutanota.com>
2022-06-29 14:21:04 +0300

    styling index

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,22 +1,44 @@
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import React, { useContext } from 'react';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>IndexScreen</Text>
       <Button title='нов пост' onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
-        renderItem={({ item }) => (<Text>{item.title}</Text>)} />
+        renderItem={({ item }) => {
+          return <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Feather style={styles.icon} name="trash-2" />
+          </View>
+        }} />
     </View >
   )
 }
 
 export default IndexScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: 'grey',
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24,
+    color: "black"
+  },
+});
