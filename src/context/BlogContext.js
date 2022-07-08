e5742c6154c14098edbbfe40c00a58effaa5abac
vladotoka <vladimir.k.terziev@tutanota.com>
2022-07-08 18:18:35 +0300

    post edit working

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -12,6 +12,10 @@ const blogReducer = (state, action) => {
                 content: action.payload.content
             }
             ];
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost;
+            });
         default:
             return state;
     }
@@ -20,7 +24,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
     return (title, content, callback) => {
         dispatch({ type: 'add_blogpost', payload: { title, content } });
-        callback();
+        if (callback) {
+            callback();
+        }
     }
 };
 
@@ -30,7 +36,16 @@ const deleteBlogPost = (dispatch) => {
     }
 };
 
+const editBlogPost = (dispatch) => {
+    return (id, title, content, callback) => {
+        dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+        if (callback) {
+            callback();
+        }
+    }
+}
+
 export const { Context, Provider } = createDataContext(blogReducer,
-    { addBlogPost, deleteBlogPost },
+    { addBlogPost, deleteBlogPost, editBlogPost },
     [{ title: 'TEST POST', content: 'TEST CONTENT', id: 42 }]
 );
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -3,15 +3,16 @@ import React, { useContext } from 'react';
 import { Context } from '../context/BlogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
-const EditScreen = ({ route }) => {
-    const { state } = useContext(Context);
+const EditScreen = ({ route, navigation }) => {
+    const { state, editBlogPost } = useContext(Context);
+    const id = route.params.id;
 
-    const blogPost = state.find((post) => post.id === route.params.id);
+    const blogPost = state.find((post) => post.id === id);
 
     return <BlogPostForm
         edit
         initialValues={{ title: blogPost.title, content: blogPost.title }}
-        onSubmit={(title, content) => console.log(title, content)}
+        onSubmit={(title, content) => editBlogPost(id, title, content, () => navigation.goBack())}
     />
 };
 
